496f439a56f25d4eaf22ccd558034ef7a018bba8
xxbeann <yjb4945@gmail.com>
2025-10-18 12:04:35 +0900

    refactor: 덧셈 기능에서 문자열 정규화 책임분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,19 +6,25 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
-        int result = add(input);
+        int result = add(normalizeInput(input));
         System.out.println("결과 : " + result);
     }
-
-    private static int add(String input) {
+    private static int[] normalizeInput(String input) {
         String[] tokens = input.split("[,:]");
-        int sum = 0;
+        int[] normalized = new int[tokens.length];
         for (int i = 0; i < tokens.length; i++) {
             if (tokens[i].isEmpty()) {
                 tokens[i] = "0";
             }
-            int n = Integer.parseInt(tokens[i]);
-            sum += n;
+            normalized[i] = Integer.parseInt(tokens[i]);
+        }
+        return normalized;
+    }
+
+    private static int add(int[] input) {
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
         }
         return sum;
     }
