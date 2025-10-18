1978e33671f6b7623ca6ec02a7b6f55e588a36c4
xxbeann <yjb4945@gmail.com>
2025-10-18 13:18:41 +0900

    feat: 커스텀 구분자 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,22 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
-        String customSeparator = getCustomSeparator(input);
-        System.out.println(customSeparator);
-        //int result = add(normalizeInput(input));
-        //System.out.println("결과 : " + result);
+        int result = add(normalizeInput(input));
+        System.out.println("결과 : " + result);
     }
+
     private static int[] normalizeInput(String input) {
         if(input.startsWith("//")) {
             String customSeparator = getCustomSeparator(input);
+            String tokens = input.split("\\\\n")[1];
+            return normalizeCustomInput(tokens,customSeparator);
         }
         String[] tokens = input.split("[,:]");
         int[] normalized = new int[tokens.length];
@@ -25,6 +28,19 @@ public class Application {
         }
         return normalized;
     }
+
+    private static int[] normalizeCustomInput(String input, String customSeparator) {
+        String[] tokens = input.split(Pattern.quote(customSeparator));
+        int[] normalized = new int[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].isEmpty()) {
+                tokens[i] = "0";
+            }
+            normalized[i] = Integer.parseInt(tokens[i]);
+        }
+        return normalized;
+    }
+
 
     private static String getCustomSeparator(String input) {
         String customSeparator = "";
