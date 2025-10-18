58df7f38fdc9e408edbb057e35103e834b0464c3
xxbeann <yjb4945@gmail.com>
2025-10-18 11:38:34 +0900

    feat: 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,8 +5,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String userInput = readLine();
-        // System.out.println(userInput);
-        // TODO: 프로그램 구현
+        String input = readLine();
+        int result = add(input);
+        System.out.println("결과 : " + result);
+    }
+
+    private static int add(String input) {
+        String[] tokens = input.split("[,:]");
+        int sum = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].isEmpty()) {
+                tokens[i] = "0";
+            }
+            int n = Integer.parseInt(tokens[i]);
+            sum += n;
+        }
+        return sum;
     }
 }
