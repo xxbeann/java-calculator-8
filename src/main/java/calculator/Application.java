31726a0a4e40fdfc1f5552e0e69b2477ff6f21f5
xxbeann <yjb4945@gmail.com>
2025-10-18 10:37:01 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = readLine();
+        // System.out.println(userInput);
         // TODO: 프로그램 구현
     }
 }
