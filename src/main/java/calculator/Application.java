241ac4df8abc4dcd989d6e0639917e52b7fa8848
xxbeann <yjb4945@gmail.com>
2025-10-18 21:24:08 +0900

    feat: 사용자 입력 검증 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,8 +10,27 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
+        validateAllowedCharacters(input);
         int result = sum(parseNumbers(input));
         System.out.println("결과 : " + result);
+    }
+
+    private static void validateAllowedCharacters(String input) {
+        if(input.startsWith("//")){
+            String customSeparator = extractCustomSeparator(input);
+            String numberParts = input.split("\\\\n")[1];
+
+            // 커스텀 구분자와 숫자만 허용
+            String escapedSeparator = Pattern.quote(customSeparator);
+            if(!numberParts.matches("[0-9" + escapedSeparator + "]+")) {
+                throw new IllegalArgumentException("허용되지 않은 문자가 포함되어 있습니다.");
+            }
+        } else {
+            // 기본 구분자만 사용
+            if (!input.matches("[0-9,:]*")) {
+                throw new IllegalArgumentException("허용되지 않은 문자가 포함되어 있습니다.");
+            }
+        }
     }
 
     private static int[] parseNumbers(String input) {
